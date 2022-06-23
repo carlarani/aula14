bc0814aa5bd2d09164a9081ad1c95ff935c3ab3d
Carla Rani <carla.rani@gmail.com>
2022-06-23 13:45:48 -0300

    Atualizando Program com testes corretos

--- Program.cs
+++ Program.cs
@@ -4,31 +4,31 @@
     {
         static void Main(string[] args)
         {
-            ////teste 01
-            //Aluno aluno = new Aluno();
-            //aluno.RegistrarNota();
-            //aluno.ApresentarNotas();
-            //Console.WriteLine($"A média do aluno foi:{aluno.MediaDasNotas()}.");
-            //Console.WriteLine($"O aluno está:{aluno.Estagio()}.");
-            //aluno.Atualizar();
+            //teste 01
+            Aluno aluno = new Aluno();
+            aluno.RegistrarNota();
+            aluno.ApresentarNotas();
+            Console.WriteLine($"A média do aluno foi:{aluno.MediaDasNotas()}.");
+            Console.WriteLine($"O aluno está:{aluno.Estagio()}.");
+            aluno.Atualizar();
 
-            ////teste 02
-            //Curso curso = new Curso();
-            //curso.Cadastrar();
-            //curso.Cadastrar();
-            //curso.Cadastrar();
-            //curso.Remover();
-            //curso.Atualizar();
-            //curso.ApresentarTodos();
+            //teste 02
+            Curso curso = new Curso();
+            curso.Cadastrar();
+            curso.Cadastrar();
+            curso.Cadastrar();
+            curso.Remover();
+            curso.Atualizar();
+            curso.ApresentarTodos();
 
-            ////teste 03
-            //Empresa empresa = new Empresa();
-            //empresa.Cadastrar();
-            //empresa.Cadastrar();
-            //empresa.Cadastrar();
-            //empresa.Remover();
-            //empresa.Atualizar();
-            //empresa.ApresentarTodos();
+            //teste 03
+            Empresa empresa = new Empresa();
+            empresa.Cadastrar();
+            empresa.Cadastrar();
+            empresa.Cadastrar();
+            empresa.Remover();
+            empresa.Atualizar();
+            empresa.ApresentarTodos();
 
 
             //teste 04
